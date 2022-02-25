f9c90cf29d5a56d4812d375c07bf702757b6d0b9
Matias Bensan A <mbensan@gmail.com>
2022-02-25 12:31:52 -0300

    Ejemplo de uso de AJAX para consultar rutas con Node/Express (ver index.html)

--- expressjs_intro/server_express.js
+++ expressjs_intro/server_express.js
@@ -1,13 +1,15 @@
 // import express from 'expresss';
 const express = require('express')
 const moment = require('moment')
+const fs = require('fs').promises
 const app = express()
 
+// Para manejo de archivos estáticos
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Esta es nuestra App')
-})
+// Se configura manejo de formularios
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
 
 app.get('/contacto', (req, res) => {
   res.send(`
@@ -25,6 +27,44 @@ app.get('/nosotros', (req, res) => {
 
 app.get('/hora', (req, res) => {
   res.send(moment().format('DD MMMM YYYY, hh:mm:ss a'))
+});
+
+app.get('/crear', async (req, res) => {
+  console.log(req.query)
+  // Acá vamos a colocar nuestro código
+  const nombre_archivo = `archivos/${req.query.nombre}.txt`
+  const contenido = `${moment().format('DD-MM-YYYY')} \n${req.query.contenido}`
+  
+  await fs.writeFile(nombre_archivo, contenido, 'utf-8')
+  res.send('Archivo creado')
+})
+
+app.get('/leer', async (req, res) => {
+  try {
+    await fs.readFile('archivos/inexistente.txt')
+  } catch (error) {
+    if (error.code = 'ENOENT'){
+      return res.send('Archivo inexistente')
+    }
+  }
+  res.send('Archivo Leído')
+});
+
+const destinos = []
+
+app.post('/destinos', (req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  destinos.push({
+    nombre: req.body.nombre,
+    descripcion: req.body.descripcion
+  })
+  res.send('Destino creado')
+});
+
+app.get('/destinos', (req, res) => {
+  // ejemplo de cómo retornar un JSON
+  res.json({destinos: destinos})
 });
 
 app.get('*', (req, res) => {
