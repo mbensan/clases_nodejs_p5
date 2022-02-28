6e092092d5e3876ce9847f6b56373b4ef5af43b2
Matias Bensan A <mbensan@gmail.com>
2022-02-28 11:04:52 -0300

    Ejemplo de uso de Jimp

--- npm_intro/server.js
+++ npm_intro/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const axios = require('axios');
-
+const jimp = require('jimp');
 
 const app = express();
 app.use(express.static('public'))
+
+app.get('/sepia', async (req, res) => {
+  const url = req.query.url
+  const font = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+  let img;
+  try {
+    img = await jimp.read(url)
+  } catch(error) {
+    return res.send('Imagen no se pudo abrir')
+  }
+  img.sepia()
+  await img.print(font, 10, 10, 'Después de la tormenta viene la calma')
+  await img.writeAsync('public/sepia.jpg')
+  res.send('Procesando la imagen de URL ' + url)
+});
 
 
 app.listen(3000, () => {
